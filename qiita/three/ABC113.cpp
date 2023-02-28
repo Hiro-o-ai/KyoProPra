267bb4d420682e6962be7e1e51169b974a57bebb
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2023-02-28 22:43:55 +0900

    fin ABC113

---
+++ qiita/three/ABC113.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, T, A, H, diff, tmp, res;
+
+    cin >> N >> T >> A;
+    for (int i = 0; i < N; ++i) {
+        cin >> H;
+        tmp = abs((T - H * 0.006) - A);
+        if(i == 0){
+            diff = tmp;
+            res = i + 1;
+            continue;
+        }
+        if(diff > tmp){
+            diff = tmp;
+            res = i + 1;
+        }
+    }
+
+    cout << res << endl;
+}
