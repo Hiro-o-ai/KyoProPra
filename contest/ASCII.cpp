bd7898043d7b1e51cbe226f1107da69cab96b105
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2023-03-19 21:51:06 +0900

    first con

---
+++ contest/ASCII.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int H, W;
+
+    cin >> H >> W;
+    vector<char> a {'.', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    for(int i = 0; i < H; ++i){
+        for(int j = 0; j < W; ++j){
+            int x;
+            cin >> x;
+            cout << a[x];
+        }
+        cout << endl;
+    }
+}
---
+++ qiita/four/ABC133.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int N, D;
+
+    cin >> N >> D;
+    int res = 0;
+
+    vector<vector<int>> x(N, vector<int>(D));
+
+    for(int i = 0; i < N; ++i){
+        for(int j = 0; j < D; ++j){
+            cin >> x[i][j];
+        }
+    }
+
+    for(int i = 0; i < N; ++ i){
+        for(int j = i + 1; j < N;  ++j){
+            int sum = 0;
+            for(int k = 0; k < D; ++k){
+                int diff = abs(x[i][k] - x[j][k]);
+                sum += diff * diff;
+            }
+            
+        }
+    }
+
+    // cout << x << endl;
+}
