abd8eeba5f2fdacf2ae6acad5b883b33d5095c8f
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2023-03-24 00:07:29 +0900

    fin ABC175

---
+++ qiita/four/ABC175.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> L(N +5);
+    for (int i =0; i < N; ++i) cin >> L[i];
+
+    int res = 0;
+
+    for (int i = 0; i < N -2; ++i){
+        for (int j = i + 1; j < N - 1; ++j){
+            for (int k = j + 1; k < N ; ++k){
+                if(L[i] != L[j] && L[i] != L[k] && L[j] != L[k]) {
+                    if(abs(L[i] - L[j]) < L[k] && L[k] < L[i] + L[j]) res += 1;
+                }
+            }
+        }
+    }
+
+    cout << res << endl;
+}
