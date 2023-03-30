c7afcd4a0d04bdf2bb7df35f1c1f54bb85549d07
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2023-03-30 22:33:23 +0900

    fin ABC088

---
+++ qiita/six/ABC088.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> cards(N);
+    for(auto &i: cards) cin >> i;
+    
+    sort(cards.begin(), cards.end(), greater<int>());
+    // reverse(cards.begin(), cards.end())もあり
+
+    int res = 0;
+    for(int i = 0; i < N; i += 2){
+        res += cards[i] - cards[i+1];
+    }
+
+    cout << res << endl;
+
+    return 0;
+}
